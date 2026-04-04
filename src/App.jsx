c9d35223b9c5836e137e52ac457bf89a0395f6a7
Allan Rodríguez Vargas <arodv0908@gmail.com>
2026-04-04 17:14:25 -0600

    refactor: lift search state up to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import { NavBar } from './components/NavBar'
 import { SearchInput } from './components/SearchInput'
+import { useState } from 'react'
 
 function App() {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <>
       <NavBar />
-      <SearchInput />
+      <SearchInput
+        search={search}
+        handleSearch={handleSearch} />
     </>
   )
 }
 
 export default App
+
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,12 +1,4 @@
-import { useState } from "react"
-
-export function SearchInput() {
-    const [search, setSearch] = useState('')
-
-    const handleSearch = (e) => {
-        setSearch(e.target.value)
-    }
-
+export function SearchInput({ search, handleSearch }) {
     return (
         <div className='flex justify-center'>
             <input name='searchGames' type="text" value={search} onChange={handleSearch} placeholder='The Witcher, Portal...' className='w-full max-w-md bg-gray-500 text-white rounded-md mt-8 px-2 py-2 mx-4' />
