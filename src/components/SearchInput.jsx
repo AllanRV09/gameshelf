6b0ace108a6f1ba5071057f6e45f1074d28a86d9
Allan Rodríguez Vargas <arodv0908@gmail.com>
2026-04-10 15:31:03 -0600

    feat: improve SearchInput accessibility with label

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,7 +1,19 @@
 export function SearchInput({ search, onChange }) {
     return (
         <div className='flex justify-center'>
-            <input name='searchGames' type="text" value={search} onChange={(e) => onChange(e.target.value)} placeholder='The Witcher, Portal...' className='w-full max-w-md bg-gray-500 text-white rounded-md mt-8 px-2 py-2 mx-4' />
+            <label htmlFor="searchGames" className="sr-only">
+                Buscar juegos
+            </label>
+
+            <input
+                id="searchGames"
+                name='searchGames'
+                type="text"
+                value={search}
+                onChange={(e) => onChange(e.target.value)}
+                placeholder='The Witcher, Portal...'
+                className='w-full max-w-md bg-gray-500 text-white rounded-md mt-8 px-2 py-2 mx-4'
+            />
         </div>
     )
 }
