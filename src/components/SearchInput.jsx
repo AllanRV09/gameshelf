7433abf608ac67356943008f61e35afabc0b855e
Allan Rodríguez Vargas <arodv0908@gmail.com>
2026-04-10 16:54:50 -0600

    feat: add search button with icon and styling

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,9 +1,11 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+
 /**
  * @param {{ search: string, onChange: (value: string) => void }} props
  */
 export function SearchInput({ search, onChange }) {
     return (
-        <div className='flex justify-center'>
+        <div className='flex justify-center mt-8 mx-8'>
             <label htmlFor="searchGames" className="sr-only">
                 Buscar juegos
             </label>
@@ -15,8 +17,14 @@ export function SearchInput({ search, onChange }) {
                 value={search}
                 onChange={(e) => onChange(e.target.value)}
                 placeholder='The Witcher, Portal...'
-                className='w-full max-w-md bg-gray-500 text-white rounded-md mt-8 px-2 py-2 mx-4'
+                className='w-full max-w-md bg-gray-500 text-white rounded-l-md px-2 py-2'
             />
+
+            <div className="flex justify-center items-center px-2 rounded-r-md bg-gray-600 hover:bg-gray-700 ">
+                <button>
+                    <MagnifyingGlassIcon className="size-6 fill-white/60" />
+                </button>
+            </div>
         </div>
     )
 }
